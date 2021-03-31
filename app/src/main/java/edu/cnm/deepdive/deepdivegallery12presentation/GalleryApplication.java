fe93d87c918c4931d4e961769d4672df41f7ed32
tnordquist <toddnordquist144@gmail.com>
2021-03-31 13:27:48 -0600

    Added Picasso image authorization; resources.

--- app/src/main/java/edu/cnm/deepdive/deepdivegallery12presentation/GalleryApplication.java
+++ app/src/main/java/edu/cnm/deepdive/deepdivegallery12presentation/GalleryApplication.java
@@ -1,16 +1,41 @@
 package edu.cnm.deepdive.deepdivegallery12presentation;
 
 import android.app.Application;
+import com.squareup.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
 import edu.cnm.deepdive.deepdivegallery12presentation.service.GoogleSignInService;
-public class GalleryApplication extends Application{
+import okhttp3.Interceptor.Chain;
+import okhttp3.OkHttpClient;
+
+public class GalleryApplication extends Application {
+
+  private String bearerToken;
 
   @Override
   public void onCreate() {
     super.onCreate();
+    setupSignIn();
+    setupPicasso();
+  }
+
+  private void setupSignIn() {
     GoogleSignInService.setContext(this);
+    GoogleSignInService.getInstance().getBearerToken().observeForever((bearerToken) -> this.bearerToken = bearerToken);
+  }
+
+  private void setupPicasso() {
+    OkHttpClient client = new OkHttpClient.Builder()
+        .addInterceptor((Chain chain) ->
+            chain.proceed(
+                chain.request().newBuilder()
+                    .addHeader("Authorization", bearerToken)
+                    .build()
+            )
+        )
+        .build();
     Picasso.setSingletonInstance(
         new Picasso.Builder(this)
+            .downloader(new OkHttp3Downloader(client))
             .loggingEnabled(BuildConfig.DEBUG)
             .build()
     );
